fcc00af1f443beac41bf8d3d727a38b7b106ce6f
Bartosz Panek <m.panekbartosz@gmail.com>
2025-03-02 20:48:04 +0100

    update loading spinner

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -171,7 +171,11 @@ export default function Todo() {
   });
 
   if (loading) {
-    return <div className="text-center">Loading...</div>;
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-gray-50">
+        <div className="h-8 w-8 animate-spin rounded-full border-2 border-red-600 border-t-transparent" />
+      </div>
+    );
   }
 
   return (
